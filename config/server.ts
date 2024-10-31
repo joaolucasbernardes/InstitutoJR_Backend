ada0a7815fdc9cebb26fe7b84efa8bf895ce1f42
Joao Lucas <joaolucasbernardes@gmail.com>
2024-10-31 08:50:39 -0300

    Atualização Geral

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,9 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('STRAPI_ADMIN_BACKEND_URL'),
+  admin: {
+    path: '/dist/build',
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
